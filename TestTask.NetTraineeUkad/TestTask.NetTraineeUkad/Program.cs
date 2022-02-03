eaba162027e9c90427061134baf95fadfa7cb220
Nikita <doofeee@gmail.com>
2022-02-03 22:35:29 +0200

    made parser

--- TestTask.NetTraineeUkad/TestTask.NetTraineeUkad/Program.cs
+++ TestTask.NetTraineeUkad/TestTask.NetTraineeUkad/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Text;
+using System.Data.Odbc;
+using System.Threading.Tasks;
 
 namespace TestTask.NetTraineeUkad
 {
@@ -6,7 +13,51 @@ namespace TestTask.NetTraineeUkad
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            startCrawlerasync();
+            Console.ReadLine();
         }
+
+        private static async Task startCrawlerasync()
+        {
+            var url = "https://github.com/";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            Dictionary<string, string> list = new Dictionary<string, string>();
+
+            HtmlNodeCollection nodes = htmlDocument.DocumentNode.SelectNodes("//a[@href]");
+
+            foreach (var n in nodes)
+            {
+                string href = n.Attributes["href"].Value;
+                try
+                {
+                    list.Add(GetAbsoluteUrlString(url, href), "");
+                }
+                catch
+                {
+                    
+                }
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+
+        static string GetAbsoluteUrlString(string baseUrl, string url)
+        {
+            var uri = new Uri(url, UriKind.RelativeOrAbsolute);
+            if (!uri.IsAbsoluteUri)
+                uri = new Uri(new Uri(baseUrl), uri);
+            return uri.ToString();
+        }
+
+
+
     }
 }
+
