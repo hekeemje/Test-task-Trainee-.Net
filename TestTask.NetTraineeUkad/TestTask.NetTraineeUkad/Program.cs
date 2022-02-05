f9f03c6804813071b028620ff99a7cb6249423af
Nikita <doofeee@gmail.com>
2022-02-05 23:35:32 +0200

    changes for check website

--- TestTask.NetTraineeUkad/TestTask.NetTraineeUkad/Program.cs
+++ TestTask.NetTraineeUkad/TestTask.NetTraineeUkad/Program.cs
@@ -9,7 +9,7 @@ namespace TestTask.NetTraineeUkad
         static async Task Main(string[] args)
         {
             using var webCrawler = new webCrawler();
-            var links = await webCrawler.startCrawler("https://github.com/");
+            var links = await webCrawler.startCrawler("https://seoagilitytools.com/");
             Console.WriteLine(new string('-', 80));
             Console.WriteLine(string.Join(Environment.NewLine, links));
             sitemapCrawler sitemap = new sitemapCrawler();
